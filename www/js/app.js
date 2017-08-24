27b17aad0af3e2db7115d075f9fb174ce15738c6
Karura28 <vanessatrevizo10@gmail.com>
2017-08-24 09:37:38 -0600

    Initial commit adding structure of directories and creating the first file

--- www/js/app.js
+++ www/js/app.js
@@ -1,22 +1,22 @@
 var base = 5;
 var altura = 7;
 var area = base * altura;
-console.log("el area de mi rectangulo es igual:" + area );
+console.log("el área de mi rectangulo es igual:" + area );
 
 
 var pi = 3.1416;
 var radio = 5;
 var area = ((pi* (radio*radio)))
-console.log("El area del circulo es :"  + area ); 
+console.log("El área del circulo es :"  + area ); 
 
 
 var base = 8.3;
 var altura = 13;
 var area = ((base*altura)/2);
-console.log("El area del Triangulo es:"  + area);
+console.log("El área del Triangulo es:"  + area);
 
 var radio = 7;
 var pi = 3.1416;
 var area = ((4/3)*pi*(radio*radio*radio));
-console.log("El area de la esfera es" +  area);
+console.log("El volumen de la esfera es" +  area);
 
